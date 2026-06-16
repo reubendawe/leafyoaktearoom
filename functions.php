199f90589dbdcac9d9ec786ba9c14947ce2d4d21
Reuben <reubendawemarketing@gmail.com>
2026-06-16 17:05:19 +0100

    Bootstrap added to functions file

--- functions.php
+++ functions.php
@@ -33,4 +33,18 @@ function leafyoak_theme_setup() {
 }
 add_action('after_setup_theme', 'leafyoak_theme_setup');
 
+// Bootstrap
+
+function leafyoak_bootstrap_assets() {
+    // 1. Enqueue Bootstrap CSS from CDN
+    wp_enqueue_style('bootstrap-cdn', 'https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css', array(), '5.3.3');
+
+    // 2. Your existing theme styles (Make sure this loads AFTER Bootstrap so you can override it)
+    wp_enqueue_style('leafyoak-custom-styles', get_template_directory_uri() . '/dist/css/main.css', array(), '1.0.0');
+
+    // 3. Enqueue Bootstrap JS Bundle (includes Popper for tooltips/dropdowns)
+    wp_enqueue_script('bootstrap-js', 'https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js', array(), '5.3.3', true);
+}
+add_action('wp_enqueue_scripts', 'leafyoak_bootstrap_assets');
+
 ?>

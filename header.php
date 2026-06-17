06b4a39bd0f90a6a85a741e1f4a33e59cfb58c0d
Reuben <reubendawemarketing@gmail.com>
2026-06-17 17:30:20 +0100

    Colour variables successfully pulling through

--- header.php
+++ header.php
@@ -4,6 +4,10 @@
 	<meta charset="<?php bloginfo( 'charset' ); ?>">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Crimson+Text:ital,wght@0,400;0,600;1,400&family=Playfair+Display:ital,wght@0,400;0,700;1,400&display=swap" rel="stylesheet">
+
   <?php wp_head(); ?>
 </head>
 
